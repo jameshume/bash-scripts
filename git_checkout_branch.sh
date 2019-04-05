796af288153868837789862d9baebf4b60057e06
James Hume <james.hume@gmail.com>
2019-04-05 16:54:06 +0100

    Small cleanup of syntax used

--- git_checkout_branch.sh
+++ git_checkout_branch.sh
@@ -2,19 +2,19 @@
 
 i=1
 branches=()
-while read branch_name;
+while read -r branch_name;
 do
 	branch_name_cleaned=$(sed -e 's/^\*\s*//g' <<< "$branch_name")
-	branches+=($branch_name_cleaned)
+	branches+=("$branch_name_cleaned")
 	echo "$i) $branch_name_cleaned"
 	((i=i+1))
-done <<< $(git branch)
+done <<< "$(git branch)"
 
 ip=0
 while [ $ip -lt 1 ] || [ $ip -gt ${#branches[*]} ]
 do
 	echo "Enter branch number: "
-	read ip
+	read -r ip
 done
 
 git checkout "${branches[$((ip-1))]}"
