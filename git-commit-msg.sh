354c0cf4d35a5bad49795bfbfad9f24390d258a1
James Hume <james.hume@gmail.com>
2019-05-15 12:58:55 +0100

    A git commit hook to check that the commit msg starts with a ticket number - implementation bit shit

---
+++ git-commit-msg.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+##
+## Rename me to git-commit in your .git/hooks directory or add me to the existing script
+##
+## Change "TICKET" in the text below to your specific prefix and adjust the \{6\} to the number
+## of digits after - it expectes a "tag" like TICKET-123456
+
+parse_git_branch() {
+   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/' | sed -e 's/^\s*//g'
+}
+
+# Parse the git branch name and fund the TICKET-xxxxxx specifier if it exists
+BRANCH=$(parse_git_branch)
+BRANCH_TICKET_NUM=$(sed -e 's/.*\(\(ticket\|TICKET\)-[0-9]\{6\}\).*/\1/g' <<< "#$BRANCH")
+[ "${BRANCH_TICKET_NUM:0:1}" = "#" ] && BRANCH_TICKET_NUM=""
+
+# Make sure the message starts with TICKET-dddddd where d is a number 0-9
+grep -i "^TICKET-[0-9]\{6\}" "$1" > /dev/null 2>&1 || {
+        echo >&2 Missing ticket number at start of message.
+        exit 1
+}
+
+MSG_TICKET_NUM=$(sed 's/^\(ticket\|TICKET\)\(-[0-9]\{6\}\).*/\1\2/g' "$1")
+
+# If the branch name exists the TICKET of the branch name must be verfied against the msg
+if [ -n "$BRANCH_TICKET_NUM" ]; then
+        if [ "$MSG_TICKET_NUM" != "$BRANCH_TICKET_NUM" ]; then
+                echo >&2 "The branch TICKET number ($BRANCH_TICKET_NUM) does not match the commit msg TICKET number ($MSG_TICKET_NUM)."
+                exit 1
+        fi
+fi
+
+echo "COMMIT @ $(date) on branch $BRANCH ($TICKET_NUM) with msg \"$(cat $1)\"" >> ~/git_commits_log.txt
